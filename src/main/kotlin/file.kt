7406be1147aec45db82aef1d9465a687618e58ce
Nikita Novgortcev <n42730689@gmail.com>
2025-11-25 11:21:42 +0300

    KTB-02-добавил обработку ошибок

--- src/main/kotlin/file.kt
+++ src/main/kotlin/file.kt
@@ -5,8 +5,11 @@ import java.io.File
 fun main() {
 
     val wordsFile = File("words.txt")
-    // wordsFile.createNewFile()
-    for (i in wordsFile.readLines()) {
-        println(i)
+    if (wordsFile.exists()) {
+        for (line in wordsFile.readLines()) {
+            println(line)
+        }
+    } else {
+        println("Файла не существует")
     }
 }
