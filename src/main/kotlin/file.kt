1d62edb81f85aa9659d7cf850392f23bfafd485e
Nikita Novgortcev <n42730689@gmail.com>
2025-11-26 15:19:05 +0300

    KTB-02-распечатал содержимое словаря из списка

--- src/main/kotlin/file.kt
+++ src/main/kotlin/file.kt
@@ -1,14 +1,22 @@
 package org.example
 
+import org.example.data.Word
 import java.io.File
 
 fun main() {
 
     val wordsFile = File("words.txt")
+    val dictionary = mutableListOf<Word>()
     if (wordsFile.exists()) {
+
         for (line in wordsFile.readLines()) {
-            println(line)
+            val line = line.split("|")
+            val word = Word(original = line[0], translate = line[1], correctAnswersCount = line[2].toIntOrNull()?:0)
+            dictionary.add(word)
         }
+       dictionary.forEach {
+           println(it)
+       }
     } else {
         println("Файла не существует")
     }
