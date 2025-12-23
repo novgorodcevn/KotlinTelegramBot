1c65478774d83311740400fed999567f57766f33
Nikita Novgortcev <n42730689@gmail.com>
2025-12-23 12:31:53 +0300

    KTB-14-Переписал парсинг update_id

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -16,12 +16,11 @@ fun main(args: Array<String>) {
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        updateId = updateIdString.toInt() + 1
+        val messageTextRegex: Regex = "\"update_id\":(.+?),".toRegex()
+        val mathResult: MatchResult? = messageTextRegex.find(updates)
+        val groups = mathResult?.groups
+        val text = groups?.get(1)?.value
+        updateId = text?.toInt()?.plus(1) ?: continue
     }
 }
 
