9572a19dcb75338b314800cf3d0d204c5dc6acd7
Nikita Novgortcev <n42730689@gmail.com>
2026-02-18 16:00:33 +0300

    KTB-25-добавил тест resetProgress() with 2 words in dictionary

--- src/test/kotlin/LearnWordsTrainerTest.kt
+++ src/test/kotlin/LearnWordsTrainerTest.kt
@@ -3,6 +3,7 @@ import org.example.NUMBER_UNLEARNED_WORDS
 import org.example.Statistics
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.assertThrows
+import java.io.File
 import kotlin.test.Test
 
 class LearnWordsTrainerTest {
@@ -19,14 +20,14 @@ class LearnWordsTrainerTest {
     @Test
     fun `test statistics with corrupted file`() {
         val exception = assertThrows<IllegalStateException> {
-            LearnWordsTrainer("src/test/4_words_of_7.txt")
+            LearnWordsTrainer("src/test/words_corrupted.txt")
         }
         assertEquals("некорректный файл", exception.message)
     }
 
     @Test
-    fun `fun test getNextQuestion() with 1 unleard word`() {
-        val trainer = LearnWordsTrainer("src/test/4_words_of_7.txt")
+    fun `fun test getNextQuestion() with 1 unlearned word`() {
+        val trainer = LearnWordsTrainer("src/test/1_unlearned_word.txt")
         kotlin.test.assertEquals(
             NUMBER_UNLEARNED_WORDS,
             trainer.getNextQuestion()?.variants?.size
@@ -34,22 +35,49 @@ class LearnWordsTrainerTest {
     }
 
     @Test
-    fun `test getNextQuestion() with all words learned`()  {
+    fun `test getNextQuestion() with all words learned`() {
         val trainer = LearnWordsTrainer("src/test/4_words_of_7.txt")
         kotlin.test.assertEquals(
             null,
-            trainer.getNextQuestion()?.variants?.size
+            trainer.getNextQuestion()
         )
     }
 
     @Test
     fun `test checkAnswer() with true`() {
         val trainer = LearnWordsTrainer("src/test/4_words_of_7.txt")
-        val correctAnswerId = trainer.getNextQuestion()?.variants?.indexOfFirst { it.original == trainer.question?.correctAnswer?.original }
+        val question = trainer.getNextQuestion()
+        trainer.question = question
+        val index = trainer.question?.correctAnswer?.original
+        val correctAnswerIndex = trainer.question?.variants?.indexOfFirst { it.original == index }
+        val result = trainer.checkAnswer(correctAnswerIndex)
         kotlin.test.assertEquals(
             true,
-            trainer.checkAnswer(correctAnswerId)
+            result
         )
     }
 
+    @Test
+    fun `test checkAnswer() with false`() {
+        val trainer = LearnWordsTrainer("src/test/4_words_of_7.txt")
+        val question = trainer.getNextQuestion()
+        trainer.question = question
+        val index = trainer.question?.correctAnswer?.original
+        val correctAnswerIndex = trainer.question?.variants?.indexOfFirst { it.original != index }
+        val result = trainer.checkAnswer(correctAnswerIndex)
+        kotlin.test.assertEquals(
+            false,
+            result
+        )
+    }
+
+    @Test
+    fun `test resetProgress() with 2 words in dictionary`() {
+        val trainer = LearnWordsTrainer("src/test/reset_word.txt")
+        trainer.resetProgress()
+        val allZero = trainer.dictionary.all { it.correctAnswersCount == 0 }
+        assertTrue(
+            allZero
+            )
+    }
 }
